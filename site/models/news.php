a2da2b1890a14bf8a3bed23609211231a9264cb8
kepler71 <kellerbm71@gmail.com>
2015-03-03 23:09:51 +0400

    Автозагрузка, и абстрактная модель

--- site/models/news.php
+++ site/models/news.php
@@ -1,20 +1,13 @@
 <?php
-require_once __DIR__.'/../classes/db.php';
-class News {
+
+class News
+    extends AbstractModel
+{
 
     public $id;
     public $title;
     public $text;
 
-     public static function  getAll(){
-        $db = new db;
-        return $db->queryAll('Select * from news','News');
-
-    }
-
-    public static function getOne($id){
-        $db = new db();
-        return $db->queryOne('SELECT * FROM news WHERE id='. $id,'News');
-
-    }
-}
+    protected static $table = 'news';
+    protected static $class = 'News';
+   }
