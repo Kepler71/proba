ee202a7f25ae8b4ac232867a005e89fd523882ac
kepler71 <kellerbm71@gmail.com>
2015-03-10 23:48:42 +0400

    Добавлен метод add

--- site/classes/AbstractModel.php
+++ site/classes/AbstractModel.php
@@ -1,18 +1,75 @@
 <?php
 
+
 abstract class AbstractModel
 {
-    protected static $table;
+    static protected $ntable;
+    static protected $likeUserTable;
 
-    public static function  getAll(){
-        $db = new db;
-        return $db->queryAll('Select * from ' . static::$table,'News');
+    protected $data = [];
+
+    public function __set($k, $v)
+    {
+        $this->data[$k] = $v;
 
     }
-    public static function getOne($id){
-        $db = new db();
-        $sql = 'SELECT * FROM' . static::$table . ' WHERE id='. $id;
-        return $db->queryOne($sql, static::$class);
+
+    public function __get($k)
+    {
+        return $this->data[$k];
+
+    }
+
+    public static function getNtable()
+    {
+        return static::$ntable;
+    }
+
+    public static function findAll()
+    {
+        $class = get_called_class();
+        $sql = 'SELECT * FROM ' . static::$ntable;
+        $db = new DB();
+        $db->setClassName($class);
+        return $db->query($sql);
+    }
+
+    public static function findOne($id)
+    {
+        $class = get_called_class();
+
+        $sql = 'SELECT * FROM ' . static::$ntable . ' WHERE id=:id';
+        $db = new DB();
+        $db->setClassName($class);
+        return $db->query($sql, [':id' => $id]) [0];
+    }
+
+    public static function findByColum ($colum, $value){
+        $class = get_called_class();
+        $sql = 'SELECT * FROM ' . static::$ntable . ' WHERE ' . colum . ' = :value';
+        $db = new DB();
+        $db->setClassName($class);
+        $db->query($sql, [':value' => $value ]);
+    }
+
+
+    public function add()
+    {
+        $cols = array_keys($this->data);
+        $ins = [];
+        $data = [];
+
+        foreach ($cols as $col){
+            $ins[] = ':' . $col;
+            $data[':' . $col] = $this->data[$col];
+        }
+       $sql = 'INSERT INTO ' . static::$ntable . '
+        (' . implode(', ',$cols) . ')
+        VALUE
+        (' . implode(', ',$ins) . ')';
+
+        $db = new DB();
+        $db->execut($sql , $data);
 
     }
 }
